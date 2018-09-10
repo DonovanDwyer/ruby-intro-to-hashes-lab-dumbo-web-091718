d360a73cd8418385b290d664d2c17a7ee9b79729
Donovan Dwyer <dwyer.donovan@gmail.com>
2018-09-10 19:02:07 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -11,7 +11,10 @@ def monopoly
 end
 
 def monopoly_with_second_tier
-	monopoly = {:railroads => {value: 4}}
+	monopoly = {:railroads => {
+	  :value => 4
+	  }
+	}
 end
 
 def monopoly_with_third_tier
